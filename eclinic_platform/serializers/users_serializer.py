af5aa78718b62ccf5204a218087c43ae21247ea7
Omoyele Samuel <samuelomoyele777@gmail.com>
2021-11-28 02:47:07 +0100

    update

--- eclinic_platform/serializers/users_serializer.py
+++ eclinic_platform/serializers/users_serializer.py
@@ -5,5 +5,27 @@ class UserSerializer(serializers.ModelSerializer):
            
     class Meta:
        model = User
-       fields = ['id', 'header_title', 'background_image', 'image', 'username', 'title', 'first_name', 'last_name', 'category', 'location_address', 'location_lat', 'location_lng', 'location_address_component', 'created_at', 'updated_at' ]
-    #    fields = '__all__'
+      #  fields = ['id', 'header_title', 'background_image', 'image', 'username', 'title', 'first_name', 'last_name', 'category', 'location_address', 'location_lat', 'location_lng', 'location_address_component', 'created_at', 'updated_at' ]
+       fields = '__all__'
+
+
+
+
+class SearchSerializer(serializers.ModelSerializer):
+           
+    class Meta:
+       model = User
+      #  fields = ['id', 'header_title', 'background_image', 'image', 'username', 'title', 'category', 'location_address', 'location_lat', 'location_lng', 'location_address_component' ]
+       fields = '__all__'
+
+       name = serializers.SerializerMethodField()
+
+       def get_name(self, instance):
+          return instance.first_name+" "+instance.last_name
+
+       def to_representation(self, instance):
+           return {
+              "username": instance.username,
+              "name": self.get_name()
+           }
+           
